557bfab8b2a1b9c9071fdf7663825c5c0cdee6fd
KoaLaYT <hytohjen@gmail.com>
2025-02-27 17:37:33 +0800

    sim8086: include casey's reference impl of sim86

--- sim8086/build.zig
+++ sim8086/build.zig
@@ -1,19 +1,27 @@
 const std = @import("std");
 
-// Although this function looks imperative, note that its job is to
-// declaratively construct a build graph that will be executed by an external
-// runner.
 pub fn build(b: *std.Build) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
     const target = b.standardTargetOptions(.{});
-
-    // Standard optimization options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
-    // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
+
+    const dump_decode = b.addExecutable(.{
+        .name = "dumpdecode",
+        .root_source_file = b.path("src/dump_decode.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const sim86_lib = b.addStaticLibrary(.{
+        .name = "sim86",
+        .root_source_file = b.path("src/sim86.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    sim86_lib.addCSourceFile(.{
+        .file = b.path("vendor/perfaware/sim86/sim86_lib.cpp"),
+        .flags = &[_][]const u8{},
+    });
+    sim86_lib.linkLibCpp();
 
     const exe = b.addExecutable(.{
         .name = "sim8086",
@@ -21,35 +29,35 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     });
+    exe.linkLibrary(sim86_lib);
 
-    // This declares intent for the executable to be installed into the
-    // standard location when the user invokes the "install" step (the default
-    // step when running `zig build`).
+    b.installArtifact(dump_decode);
+    b.installArtifact(sim86_lib);
     b.installArtifact(exe);
 
-    // This *creates* a Run step in the build graph, to be executed when another
-    // step is evaluated that depends on it. The next line below will establish
-    // such a dependency.
-    const run_cmd = b.addRunArtifact(exe);
+    const dump_decode_cmd = b.addRunArtifact(dump_decode);
+    const exe_cmd = b.addRunArtifact(exe);
 
-    // By making the run step depend on the install step, it will be run from the
-    // installation directory rather than directly from within the cache directory.
-    // This is not necessary, however, if the application depends on other installed
-    // files, this ensures they will be present and in the expected location.
-    run_cmd.step.dependOn(b.getInstallStep());
+    dump_decode_cmd.step.dependOn(b.getInstallStep());
+    exe_cmd.step.dependOn(b.getInstallStep());
 
-    // This allows the user to pass arguments to the application in the build
-    // command itself, like this: `zig build run -- arg1 arg2 etc`
     if (b.args) |args| {
-        run_cmd.addArgs(args);
+        dump_decode_cmd.addArgs(args);
+        exe_cmd.addArgs(args);
     }
 
-    // This creates a build step. It will be visible in the `zig build --help` menu,
-    // and can be selected like this: `zig build run`
-    // This will evaluate the `run` step rather than the default, which is "install".
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
+    const run_dump_decode_step = b.step("dumpdecode", "Run the dump decode");
+    run_dump_decode_step.dependOn(&dump_decode_cmd.step);
 
+    const run_exe_step = b.step("run", "Run the sim");
+    run_exe_step.dependOn(&exe_cmd.step);
+
+    const sim86_tests = b.addTest(.{
+        .root_source_file = b.path("src/sim86_test.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    sim86_tests.linkLibrary(sim86_lib);
     const exe_unit_tests = b.addTest(.{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
@@ -57,10 +65,9 @@ pub fn build(b: *std.Build) void {
     });
 
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+    const run_sim86_tests = b.addRunArtifact(sim86_tests);
 
-    // Similar to creating the run step earlier, this exposes a `test` step to
-    // the `zig build --help` menu, providing a way for the user to request
-    // running the unit tests.
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_exe_unit_tests.step);
+    test_step.dependOn(&run_sim86_tests.step);
 }
